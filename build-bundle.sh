3cc0e31e75b2420c54c830df911cd444575cd577
John O'Sullivan <john@eximchain.com>
2019-08-09 16:18:58 -0400

    Upgraded build script to work without Typescript installed

--- build-bundle.sh
+++ build-bundle.sh
@@ -4,12 +4,14 @@ BUNDLE_NAME=$1
 
 # Install and copy over node_modules
 npm install
-rm -rf build/node_modules
+rm -rf build/*
 cp -R node_modules build/node_modules
 
 # Build, enter, and zip
 rm -f $BUNDLE_NAME
-tsc
+
+# npx makes command work even if tsc isn't installed
+npx -p typescript tsc
 cd build
 zip -r ../$BUNDLE_NAME *
 cd ..
